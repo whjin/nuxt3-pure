88d204373f4a572649efb5503b52b2087beb53b5
whjin <wuhuajin09@163.com>
2024-05-05 15:57:47 +0800

    deploy: 部署至github pages

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <NuxtLink to="/memo">Memo</NuxtLink>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+export default {
+  props: {},
+  setup (props) { },
+};
+</script>
+
+<style lang="less"></style>
---
+++ pages/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+export default {
+  props: {},
+  setup (props) { }
+};
+</script>
+
+<style lang="less"></style>
