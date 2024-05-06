bab81e3b3761263f946c713f6f5e126dda553c79
whjin <wuhuajin09@163.com>
2024-05-06 23:52:29 +0800

    Updates

--- static/C7dU3E_E.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./v-OzSCxK.js";import{_ as s,c as n,b as a,w as c,o as _,a as r}from"./DqGIS7_I.js";const p={props:{},setup(o){}},i={class:"container"},d=r("h1",null,"Home",-1);function u(o,m,f,l,x,h){const t=e;return _(),n("div",i,[a(t,{to:"/"},{default:c(()=>[d]),_:1})])}const B=s(p,[["render",u]]);export{B as default};
